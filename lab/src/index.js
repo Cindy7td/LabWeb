8a72d999d21e9420b7b4d0e49a0aa7cae4ee2d3c
yess_37 <yessica.hdez14@gmail.com>
2021-06-06 23:01:48 -0600

    firebase_config

--- lab/src/index.js
+++ lab/src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+firebase.auth().signOut().then(() => {
+  // Sign-out successful.
+}).catch((error) => {
+  // An error happened.
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
